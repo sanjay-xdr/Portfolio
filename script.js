1f8ed2929ef1241d93aa5a4514ae6a402f66894d
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2023-04-26 09:06:59 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
+}
+
+function drop(event) {
+    var offset = event.dataTransfer.getData("text/plain").split(',');
+    var dm = document.getElementsByClassName('dragme');
+    dm[parseInt(offset[2])].style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+    dm[parseInt(offset[2])].style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+    event.preventDefault();
+    return false;
+}
+
+var dm = document.getElementsByClassName('dragme');
+for (var i = 0; i < dm.length; i++) {
+    dm[i].addEventListener('dragstart', drag_start, false);
+    document.body.addEventListener('dragover', drag_over, false);
+    document.body.addEventListener('drop', drop, false);
+}
+
+
+const projects=document.querySelector("#projects");
+projects.addEventListener('click',()=>{
+    window.location.href="https://www.google.com"
+})
